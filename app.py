0dc8e6e13c274a0512aac9b11be91d18a80c2b17
ZachDosntKnowWhatHesDoing <techeyboyyt@gmail.com>
2025-09-10 16:49:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -250,5 +250,5 @@ def delete_post(post_id):
         save_json(POSTS_FILE, {'posts': posts})
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
